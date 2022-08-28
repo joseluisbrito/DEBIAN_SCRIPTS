ea89964a66c97293f71843a5f27811bfca648270
José Brito <joseluisbrito@gmail.com>
2022-08-27 21:33:02 -0300

    Added ipv6 disable script for Debian

---
+++ disable_ipv6_debian.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# DISABLE IPv6 SCRIPT | By José Brito | 2022
+
+sed -i '/net.ipv6.conf.all.disable_ipv6/d' /etc/sysctl.conf
+sed -i '/net.ipv6.conf.default.disable_ipv6/d' /etc/sysctl.conf
+sed -i '/net.ipv6.conf.lo.disable_ipv6/d' /etc/sysctl.conf
+
+echo 'net.ipv6.conf.all.disable_ipv6=1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.default.disable_ipv6=1' >> /etc/sysctl.conf
+echo 'net.ipv6.conf.lo.disable_ipv6=1' >> /etc/sysctl.conf
+
+/sbin/sysctl -p
